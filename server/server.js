524200f530e5297e434511fd55c96c84ebbb8818
Srishti Luthra <srishtil@umich.edu>
2025-12-14 23:29:01 -0500

    update server with corrected code

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
 
 import { db } from './db/db.js';
 import { productRouter } from './routes/productRoutes.js';
@@ -10,9 +13,26 @@ import customCakeRouter from './routes/customCakeRoute.js';
 const port = 6790;
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const serverPath =  path.join(__dirname, '..');
 
 app.use(cors());
 app.use(express.json());
+
+
+app.use(
+  express.static(
+    path.join(
+      serverPath, 'client/build')
+));
+
+app.get('/', (req, res) => {
+  res.sendFile(
+    path.join(
+      serverPath, 'client/build/index.html'));
+});
 
 app.use('/products', productRouter);
 app.use('/users', loginRouter);
